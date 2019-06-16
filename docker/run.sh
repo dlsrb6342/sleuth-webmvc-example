19323111294520b030a71ae1f5d5c7beb565b607
Tim te Beek <tim.te.beek@jdriven.com>
2019-06-16 17:53:35 +0200

    Shows how to trace Kafka without changing your code
    This changes the example to invoke the backend with Kafka
    instead of WebMVC. Sleuth automatically configures Brave's
    kafka-clients instrumentation when spring-kafka is present.
    
    https://github.com/openzipkin/brave/tree/master/instrumentation/kafka-clients
    https://github.com/spring-cloud/spring-cloud-sleuth/blob/v2.1.1.RELEASE/spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/messaging/TraceMessagingAutoConfiguration.java#L110

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@
 set -eou pipefail
 
 CLASS_NAME="$1"; shift
-java -cp "target/dependency/*:target/*" -Dlogging.level.org.springframework.cloud.sleuth=DEBUG -Dspring.zipkin.baseUrl=http://zipkin:9411 -Dspring.example.backendBaseUrl=http://backend:9000 sleuth.webmvc.$CLASS_NAME
+java -cp "target/dependency/*:target/*" -Dlogging.level.org.springframework.cloud.sleuth=DEBUG -Dspring.zipkin.baseUrl=http://zipkin:9411 sleuth.webmvc.$CLASS_NAME
