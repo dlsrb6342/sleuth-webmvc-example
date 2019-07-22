4b1b458bc280d98e22c1d41cbf976f3a47d03f35
ingyu.hwang <ingyu.hwang@linecorp.com>
2019-07-22 15:53:41 +0900

    With RouteFunctions

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -1,23 +1,39 @@
 package sleuth.webmvc;
 
 import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
 
 @EnableAutoConfiguration
 @RestController
 public class Backend {
 
-  @RequestMapping("/api") public String printDate() {
-    return new Date().toString();
+  public Mono<ServerResponse> printDate(ServerRequest request) {
+    String dateString = new Date().toString();
+    return ServerResponse.ok().body(BodyInserters.fromObject(dateString));
   }
+
+  @Bean
+  public RouterFunction<ServerResponse> route() {
+    return RouterFunctions.route(RequestPredicates.GET("/api"), this::printDate);
+  }
+
 
   public static void main(String[] args) {
     SpringApplication.run(Backend.class,
-        "--spring.application.name=backend",
-        "--server.port=9000"
+                          "--spring.application.name=backend",
+                          "--server.port=9000"
     );
   }
 }
--- src/main/java/sleuth/webmvc/Frontend.java
+++ src/main/java/sleuth/webmvc/Frontend.java
@@ -5,31 +5,44 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
 
 @EnableAutoConfiguration
-@RestController
 @CrossOrigin // So that javascript can be hosted elsewhere
 public class Frontend {
 
-  @Autowired RestTemplate restTemplate;
+  @Autowired WebClient webClient;
 
   String backendBaseUrl = System.getProperty("spring.example.backendBaseUrl", "http://localhost:9000");
 
-  @RequestMapping("/") public String callBackend() {
-    return restTemplate.getForObject(backendBaseUrl + "/api", String.class);
+  public Mono<ServerResponse> callBackend(ServerRequest request) {
+    return webClient.get().uri(backendBaseUrl + "/api")
+                    .retrieve().bodyToMono(String.class)
+                    .flatMap(s -> ServerResponse.ok().body(BodyInserters.fromObject(s)));
   }
 
-  @Bean RestTemplate restTemplate() {
-    return new RestTemplate();
+  @Bean WebClient webClient() {
+    return WebClient.builder().build();
   }
+
+  @Bean
+  public RouterFunction<ServerResponse> route() {
+    return RouterFunctions.route(RequestPredicates.GET("/"), this::callBackend);
+  }
+
 
   public static void main(String[] args) {
     SpringApplication.run(Frontend.class,
-        "--spring.application.name=frontend",
-        "--server.port=8081"
+                          "--spring.application.name=frontend",
+                          "--server.port=8081"
     );
   }
 }
